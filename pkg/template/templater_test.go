ce657e8e2ed7e9c9c7122fe04e9019750c13b63e
GarbhanK <garbhan@me.com>
2024-09-11 23:43:21 +0100

    unit test for refactored ReadSQL

--- pkg/template/templater_test.go
+++ pkg/template/templater_test.go
@@ -1,5 +1,10 @@
 package template
 
+import (
+	"os"
+	"testing"
+)
+
 var sql string = `select *
   from ` + "`{{ params.project }}.transactions.coffee`" + ` c
   left join` + "`{{ params.web_project }}.user_data.signup`" + ` t
@@ -8,43 +13,58 @@ var sql string = `select *
  group by insertionTimestamp desc
 `
 
-// func TestReadSQL(t *testing.T) {
-// 	// Create a temporary file
-// 	tmpFile, err := os.CreateTemp("", "test.sql")
-// 	if err != nil {
-// 		t.Fatalf("Failed to create temp file: %v", err)
-// 	}
-// 	defer os.Remove(tmpFile.Name())
+var testMapping = map[string]string{
+	"params.project":     "project123",
+	"params.web_project": "testproj",
+}
 
-// 	// Write some content to the temporary file
-// 	if _, err := tmpFile.WriteString(sql); err != nil {
-// 		t.Fatalf("Failed to write to temp file: %v", err)
-// 	}
-// 	tmpFile.Close()
+func TestReadSQL(t *testing.T) {
 
-// 	expected := sql
-// 	result, err := ReadSQL(tmpFile.Name())
-// 	if err != nil {
-// 		t.Fatalf("Failed to read file: %v", err)
-// 	}
+	// Create a temporary file
+	tmpFile, err := os.CreateTemp("", "test.sql")
+	if err != nil {
+		t.Fatalf("Failed to create temp file: %v", err)
+	}
+	defer os.Remove(tmpFile.Name())
 
-// 	if len(result) > 0 {
-// 		t.Logf("ReadSQL(%s) PASSED. Is not an empty string\n", tmpFile.Name())
-// 	} else {
-// 		t.Errorf("ReadSQL(%s) FAILED. Got an empty string\n", tmpFile.Name())
-// 	}
+	// Write some content to the temporary file
+	if _, err := tmpFile.WriteString(sql); err != nil {
+		t.Fatalf("Failed to write to temp file: %v", err)
+	}
+	tmpFile.Close()
 
-// 	// Check if the content matches what was written
-// 	if result != expected {
-// 		t.Errorf("Expected %q, got %q", expected, result)
-// 	}
-// 	if result != expected {
-// 		t.Errorf("ReadSQL(%s) FAILED.\nExpected...\n%sGot..\n%s\n", tmpFile.Name(), expected, result)
-// 	} else {
-// 		t.Logf("ReadSQL(%s) PASSED.\nExpected... \n%sGot... \n%s\n", tmpFile.Name(), expected, result)
-// 	}
+	templ := Templater{
+		Filename: tmpFile.Name(),
+		Mapping:  testMapping,
+	}
 
-// }
+	// check FileContents empty before ReadSQL call
+	if len(templ.FileContents) == 0 {
+		t.Logf("ReadSQL(%s) PASSED. Is not an empty string\n", tmpFile.Name())
+	} else {
+		t.Errorf("ReadSQL(%s) FAILED. Got an empty string\n", tmpFile.Name())
+	}
+
+	templ.ReadSQL()
+	expected := sql
+	result := templ.FileContents
+
+	if len(result) > 0 {
+		t.Logf("ReadSQL(%s) PASSED. Is not an empty string\n", tmpFile.Name())
+	} else {
+		t.Errorf("ReadSQL(%s) FAILED. Got an empty string\n", tmpFile.Name())
+	}
+
+	// Check if the content matches what was written
+	if result != expected {
+		t.Errorf("Expected %q, got %q", expected, result)
+	}
+	if result != expected {
+		t.Errorf("ReadSQL(%s) FAILED.\nExpected...\n%sGot..\n%s\n", tmpFile.Name(), expected, result)
+	} else {
+		t.Logf("ReadSQL(%s) PASSED.\nExpected... \n%sGot... \n%s\n", tmpFile.Name(), expected, result)
+	}
+}
 
 // func TestValidateSQL(t *testing.T) {
 // 	// Capture the output of the function
