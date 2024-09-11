a995ecd30dfdefc30809fa9bc36899290fa64fc3
GarbhanK <garbhan@me.com>
2024-09-11 15:36:03 +0100

    merged mapper.go into templater, refactored to be a Templater struct with methods

--- cmd/template.go
+++ cmd/template.go
@@ -18,29 +18,30 @@ var templateCmd = &cobra.Command{
 	Use:     "template",
 	Aliases: []string{"templ", "tmp", "t"},
 	Short:   "Template airflow variables and add it to system clipboard",
-	Long:    `All software has versions. This is Hugo's`,
+	Long:    `Template airflow variables and add it to system clipboard`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) < 1 {
 			fmt.Println("First argument must be a sql file")
 			os.Exit(1)
 		}
-
-		sqlFilename := args[0]
-
-		var m map[string]string
 		env, _ := cmd.Flags().GetString("env")
 
-		m = utils.CreateMapping(env)
-		m = utils.AddAirflowVars(m)
+		tConfig := utils.Templater{
+			Filename: args[0],
+			Mapping:  utils.CreateMapping(env),
+		}
 
-		// read in sql file
-		templatedSQL := utils.TemplateSQLFile(sqlFilename, m)
+		// add airflow specific template variables to the current mapping config
+		tConfig.AddAirflowVars()
 
-		// Check if the "create or replace" was left in
-		utils.ValidateSQL(templatedSQL)
+		// find and replace template variables in the file
+		tConfig.TemplateSQLFile()
+
+		// Warn if sql statement has create/insert/update/delete/drop
+		tConfig.ValidateSQL()
 
 		// Send the templated string to the clipboard (doesn't work on linux)
-		utils.ExportToClipboard(templatedSQL)
+		utils.ExportToClipboard(tConfig.FileTemplated)
 
 		var beQuiet bool
 		cfgFileQuiet := viper.GetBool("quiet")
--- pkg/utils/templater.go
+++ pkg/utils/templater.go
@@ -1,37 +1,82 @@
 package utils
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/fatih/color"
+	"github.com/spf13/viper"
 )
 
-func ReadSQL(fileName string) (string, error) {
+type Templater struct {
+	Filename      string
+	FileContents  string
+	FileTemplated string
+	Mapping       map[string]string
+}
+
+func CreateMapping(env string) map[string]string {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mappingFilePath := fmt.Sprintf("%s/Documents/flowtool/config.json", homeDir)
+	mappingFile, err := os.ReadFile(mappingFilePath)
+	if err != nil {
+		fmt.Printf("Cannot find 'mappings.json' file in path %s, %s\n", mappingFilePath, err.Error())
+		os.Exit(0)
+	}
+	m := map[string]string{}
+
+	// read json file into a map[string]string
+	json.Unmarshal([]byte(mappingFile), &m)
+
+	// env variable priority is... (--env flag > config file > default)
+	// if I have a flag set (default is dev), override config file
+	cfgFileEnv := viper.GetString("env")
+	if env == "dev" && cfgFileEnv != "" {
+		env = cfgFileEnv
+	}
+
+	// template chosen environment into our mapping file
+	for k, v := range m {
+		m[k] = strings.Replace(v, "${env}", env, -1)
+	}
+
+	return m
+}
+
+func (t *Templater) ReadSQL() {
 	// give warning if the filename doesn't have a '.sql' suffix
-	if !strings.HasSuffix(fileName, ".sql") {
+	if !strings.HasSuffix(t.Filename, ".sql") {
 		color.Yellow("WARNING - specified filename does not have '.sql' suffix!\n")
 	}
 
-	f, err := os.Open(fileName)
+	f, err := os.Open(t.Filename)
 	if err != nil {
-		return "", err
+		fmt.Printf("error reading file into memory: %s", err)
+		os.Exit(1)
 	}
 	defer f.Close()
 
 	// read file into memory
 	bytes, err := io.ReadAll(f)
 	if err != nil {
-		return "", fmt.Errorf("error reading file contents from %s", fileName)
+		fmt.Printf("error reading file contents from %s", t.Filename)
+		os.Exit(1)
 	}
 
-	return string(bytes), nil
+	t.FileContents = string(bytes)
 }
 
-func ValidateSQL(sqlFile string) {
-	formattedSQL := strings.ReplaceAll(sqlFile, "\n", " ")
+func (t Templater) ValidateSQL() {
+	formattedSQL := strings.ReplaceAll(t.FileTemplated, "\n", " ")
 	queryWords := strings.Split(formattedSQL, " ")
 
 	// Print warning to screen if it contains DDL
@@ -48,23 +93,59 @@ func ValidateSQL(sqlFile string) {
 	case "drop":
 		color.Red("DANGER - copied query is a DROP statement! Proceed with caution.\n")
 	}
-
 }
 
-// TODO: rename something like populateThing or mapConfigValues
-func TemplateSQLFile(fileName string, mapping map[string]string) string {
-	sqlFile, err := ReadSQL(fileName)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	sqlFilePtr := &sqlFile
+func (t *Templater) TemplateSQLFile() {
+	t.ReadSQL()
+
+	sqlFilePtr := &t.FileContents
 
 	var template string
-	for k, v := range mapping {
+	for k, v := range t.Mapping {
 		template = fmt.Sprintf("{{ %s }}", k)
 		*sqlFilePtr = strings.ReplaceAll(*sqlFilePtr, template, v)
 	}
 
-	return sqlFile
+	t.FileTemplated = t.FileContents
+}
+
+func (t *Templater) AddAirflowVars() {
+	// grab the current airflow date (today -1)
+	dt := time.Now().AddDate(0, 0, -1)
+	yesterday := dt.AddDate(0, 0, -1)
+	tomorrow := dt.AddDate(0, 0, 1)
+
+	// create airflow template variables ref: https://airflow.apache.org/docs/apache-airflow/stable/templates-ref.html
+	ds := fmt.Sprintf("%d-%02d-%02d", dt.Year(), dt.Month(), dt.Day())
+	ds_nodash := fmt.Sprintf("%02d%02d%02d", dt.Year(), dt.Month(), dt.Day())
+
+	ts := fmt.Sprintf("%d-%02d-%02dT%02d:%02d:%02d+00:00",
+		dt.Year(), dt.Month(), dt.Day(),
+		dt.Hour(), dt.Minute(), dt.Second())
+
+	ts_nodash := fmt.Sprintf("%d%02d%dT%02d%02d%02d+0000",
+		dt.Year(), dt.Month(), dt.Day(),
+		dt.Hour(), dt.Minute(), dt.Second())
+
+	ts_nodash_with_tz := fmt.Sprintf("%d%02d%dT%02d%02d%02d",
+		dt.Year(), dt.Month(), dt.Day(),
+		dt.Hour(), dt.Minute(), dt.Second())
+
+	yesterday_ds := fmt.Sprintf("%d-%02d-%02d", yesterday.Year(), yesterday.Month(), yesterday.Day())
+	yesterday_ds_nodash := fmt.Sprintf("%d%02d%02d", yesterday.Year(), yesterday.Month(), yesterday.Day())
+
+	tomorrow_ds := fmt.Sprintf("%d-%02d-%02d", tomorrow.Year(), tomorrow.Month(), tomorrow.Day())
+	tomorrow_ds_nodash := fmt.Sprintf("%d%02d%02d", tomorrow.Year(), tomorrow.Month(), tomorrow.Day())
+
+	t.Mapping["ds"] = ds
+	t.Mapping["ds_nodash"] = ds_nodash
+
+	t.Mapping["ts"] = ts
+	t.Mapping["ts_nodash"] = ts_nodash
+	t.Mapping["ts_nodash_with_tz"] = ts_nodash_with_tz
+
+	t.Mapping["yesterday_ds"] = yesterday_ds
+	t.Mapping["yesterday_ds_nodash"] = yesterday_ds_nodash
+	t.Mapping["tomorrow_ds"] = tomorrow_ds
+	t.Mapping["tomorrow_ds_nodash"] = tomorrow_ds_nodash
 }
